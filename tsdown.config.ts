a2a550a7393fb77dbfcc1d169b9ea12cedf9101a
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2026-03-01 17:19:13 -0500

    tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: {
     'runtime/index': 'src/index.ts',
     'vite/index': 'src/vite/index.ts',
   },
-  format: ['esm'],
   dts: true,
-  clean: true,
   sourcemap: true,
   external: ['vite', '@swc/core'],
-  treeshake: true,
 })
