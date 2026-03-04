3021e3300c0d77b4a3453c36cffcce35a23b659c
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2026-03-05 01:51:15 +0530

    ok

--- src/virtual.d.ts
+++ src/virtual.d.ts
@@ -1,4 +1,9 @@
-declare module 'virtual:catom.css' {
-  const content: string
-  export default content
+declare module "virtual:catom.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const def: any;
+  export default def;
 }
